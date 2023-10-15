06c82956758ec5fdc82ef495df018816987c31a5
nunocorverag <a19300091@ceti.mx>
2023-10-15 02:50:58 -0600

    Background image was set

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print("HOLA MUNDO")
+import pygame
+import math
+import random
+
+#Initialize the game
+pygame.init()
+
+#Set up the screen dimensions
+screen = pygame.display.set_mode((800,600))
+
+#Background
+background_image = pygame.image.load("background.jpg")
+background_image = pygame.transform.scale(background_image, (800, 600))
+
+running = True
+
+while running:
+    screen.fill((0,0,0))
+    screen.blit(background_image, (0,0))
+
+    for event in pygame.event.get():
+        # Quit the game
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            #This checks if the key pressed is the left arrow
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.4
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.4
+
+    #Update the display
+    pygame.display.update() 
